caafb27d57681b2adcae3a548eb745ef3c75bcca
Bhavik Raval <bhavman2@gmail.com>
2018-02-14 02:48:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase()===string){
     return "YES INDEED!"
   }
-  if (string===""){
+  if (string==="I love you, Grandma."){
     return "I can't hear you!"
   }
 }
