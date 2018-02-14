8648628bf47fc4384dc5656548916df63e44c36d
Bhavik Raval <bhavman2@gmail.com>
2018-02-14 02:46:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toUpperCase()===string){
-    return ""
+  if (string.toLowerCase()===string){
+    return "I can't hear you!"
   }
 }
 
